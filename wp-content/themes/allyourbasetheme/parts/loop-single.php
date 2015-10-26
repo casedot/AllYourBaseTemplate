a6c0b8df8d8a6bb2e16a08ccca0e1a8d81bcbdc2
Mex <casedot@gmail.com>
2015-10-26 00:19:38 -0700

    Recommitting some code

--- wp-content/themes/allyourbasetheme/parts/loop-single.php
+++ wp-content/themes/allyourbasetheme/parts/loop-single.php
@@ -2,17 +2,28 @@
 						
 	<header class="article-header">	
 		<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
+		<?php if (has_excerpt()) { ?><div class="entry-subtitle subtitle"><?php the_excerpt(); ?> </div> <?php } ?>
 		<?php get_template_part( 'parts/content', 'byline' ); ?>
     </header> <!-- end article header -->
 					
     <section class="entry-content" itemprop="articleBody">
-		<?php the_post_thumbnail('full'); ?>
+		<p><?php the_post_thumbnail('full'); ?></p>
 		<?php the_content(); ?>
+		<p class="normal align-center">
+			<i class="fa fa-diamond"></i>
+		</p>
 	</section> <!-- end article section -->
 						
-	<footer class="article-footer">
-		<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>	</footer> <!-- end article footer -->
+	<footer class="article-footer entry-tags">
+		<p class="tags"><?php the_tags('<span class="tags-title">TAGS</span> ', '', ''); ?></p>	</footer> <!-- end article footer -->
 									
-	<?php comments_template(); ?>	
+	<div class="row">
+		<div class="post-comments large-9 medium-8 columns">
+			<?php comments_template(); ?>
+		</div>
+		<div class="post-sidebar large-3 medium-4 columns">
+			<?php get_sidebar(); ?>
+		</div>
+	
 													
 </article> <!-- end article -->
